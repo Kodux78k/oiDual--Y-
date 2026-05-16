c35ecd05ec0fbec21d3721fa3ecf0dcf56334fbe
Kodux78k <koduxlive@gmail.com>
2026-05-15 23:58:15 -0300

    Atualizar o main.js

--- M0D/KxaT/js/main.js
+++ M0D/KxaT/js/main.js
@@ -1,11 +1,14 @@
 console.log("Infodose conectado", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
 import "./modules/inline-0.js";
+import "./modules/archs.js";
+
 import "./modules/inline-1.js";
 import "./modules/inline-2.js";
 import "./modules/inline-3.js";
 import "./modules/inline-4.js";
 import "./modules/inline-5.js";
 import "./modules/inline-6.js";
+
 import "./modules/inline-7-1.js";
 import "./modules/inline-7-2.js";
 import "./modules/inline-8.js";
