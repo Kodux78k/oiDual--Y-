6d1a84aead0702f5161e7bb64066a5992517b384
Kodux78k <koduxlive@gmail.com>
2026-05-16 01:40:13 -0300

    Update main-0.js

--- M0D/KxaT/js/main-0.js
+++ M0D/KxaT/js/main-0.js
@@ -7,7 +7,7 @@ import "./modules/inline-2.js";
 import "./modules/inline-3.js";
 import "./modules/inline-4.js";
 import "./modules/inline-5.js";
-import "./modules/inline-6.js";
+import "./modules/inline-7.js";
 
 
 import "./modules/inline-7-3.js";
