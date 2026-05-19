fbf694794d4eaf9a96fa52010e5c5d54ed545370
Kodux78k <koduxlive@gmail.com>
2026-05-18 21:44:12 -0300

    Atualizar o main.js

--- M0D/KxaT/js/main.js
+++ M0D/KxaT/js/main.js
@@ -9,7 +9,7 @@ import "./modules/inline-4.js";
 import "./modules/inline-5.js";
 
 import "./modules/inline-6.js";
-
+import "./modules/kob.js";
 
 import "./modules/inline-7-9.js";
 import "./modules/inline-8.js";
