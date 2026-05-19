247014496ab65bf3578857e4497a763d8f4bfce6
Kodux78k <koduxlive@gmail.com>
2026-05-19 20:10:06 -0300

    Update main-1.js

--- M0D/KxaT/js/main-1.js
+++ M0D/KxaT/js/main-1.js
@@ -10,7 +10,7 @@ import "./modules/inline-5.js";
 import "./modules/inline-6.js";
 
 /*import "./modules/isoCos.js";*/
-
+import "./modules/kob.js"; 
 
 import "./modules/inline-7-9.js";
 import "./modules/inline-8.js";
@@ -18,6 +18,6 @@ import "./modules/inline-9.js";
 import "./modules/inline-10.js";
 import "./modules/inline-11.js";
 
-import "./modules/kob.js"; 
 
-import "./modules/KAOS-extract.js"; 
+
+import "./modules/KAOS-extracto.js"; 
