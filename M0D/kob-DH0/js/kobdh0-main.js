ce3e4188ab078fd7e25ebceb446f5bdfffaca84c
Kodux78k <koduxlive@gmail.com>
2026-05-19 18:42:26 -0300

    Atualizar o kobdh0-main.js

--- M0D/kob-DH0/js/kobdh0-main.js
+++ M0D/kob-DH0/js/kobdh0-main.js
@@ -2,7 +2,7 @@
 
 import {} from './m0ds/kob-glue-dh10-2-oai.js';
 
-import {
+/*import {
   registerArchetypes,
   activateArchetype,
   speakWithCurrentArchetype
@@ -11,3 +11,4 @@ import {
 registerArchetypes(ARCHETYPES);
 activateArchetype('atlas');
 speakWithCurrentArchetype('O sistema está vivo.');
+*/
