16f0fbb851b65b89097f8aaf8848586005442e47
Kodux78k <koduxlive@gmail.com>
2026-05-20 03:49:19 -0300

    Add KOB-RHEA-KAOS-sync.js import and clean up
    Added import for KOB-RHEA-KAOS-sync.js and commented out unused imports.

--- M0D/KxaT/js/main-2.js
+++ M0D/KxaT/js/main-2.js
@@ -6,10 +6,12 @@ import "./modules/inline-3.js";
 import "./modules/inline-4.js";
 import "./modules/inline-5.js";
 
+import "./modules/KOB-RHEA-KAOS-sync.js";
+/*
 import "./modules/RHEA-bridge.js";
 import "./modules/KAOS-in6-0.js";
 import "./modules/inline-7-9.js";
-
+*/
 /*
 import "./modules/isoCos.js";
 import "./modules/KAOS-extracto.js"; 
