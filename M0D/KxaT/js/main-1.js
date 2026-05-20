dae0027967ce87b05e13fb39bccb3f192a44c340
Kodux78k <koduxlive@gmail.com>
2026-05-20 02:19:56 -0300

    Update main-1.js

--- M0D/KxaT/js/main-1.js
+++ M0D/KxaT/js/main-1.js
@@ -1,4 +1,4 @@
-console.log("Infodose conectado 7-9 main-1, sem kob. te amo kobllux!!! artAsCii kkkkk", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
+console.log("Infodose conectado 7-8 e com KAOS-in6-0 $$ main-1, ⚡️sem kob. te amo kobllux!!! artAsCii kkkkk", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
 import "./modules/inline-00.js";
 
 
@@ -7,14 +7,14 @@ import "./modules/inline-2.js";
 import "./modules/inline-3.js";
 import "./modules/inline-4.js";
 import "./modules/inline-5.js";
-import "./modules/inline-6-0.js";
+import "./modules/KAOS-in6-0.js";
 
 /*import "./modules/isoCos.js";
 import "./modules/KAOS-extracto.js"; 
 
 import "./modules/kob.js"; 
 */
-import "./modules/inline-7-9.js";
+import "./modules/inline-7-8.js";
 import "./modules/inline-8.js";
 import "./modules/inline-9.js";
 import "./modules/inline-10.js";
