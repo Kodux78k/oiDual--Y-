71ea435a99a1fc329a852ed4859620908035730b
Kodux78k <koduxlive@gmail.com>
2026-05-20 03:54:14 -0300

    Update main-2.js

--- M0D/KxaT/js/main-2.js
+++ M0D/KxaT/js/main-2.js
@@ -1,4 +1,5 @@
 console.log("Infodose conectado main2!! KAOS 6-0 RHEA IN 7-9 main-2!!! sem kob. te amo kobllux!!! artAsCii kkkkk", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
+import "./modules/KOB-ASCII-boot.js";
 import "./modules/inline-00.js";
 import "./modules/inline-1.js";
 import "./modules/inline-2.js";
