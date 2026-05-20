a927582517d29df145a1bc3eafc541a503ae1801
Kodux78k <koduxlive@gmail.com>
2026-05-20 02:58:01 -0300

    Update main-2.js

--- M0D/KxaT/js/main-2.js
+++ M0D/KxaT/js/main-2.js
@@ -1,17 +1,20 @@
-console.log("Infodose conectado sem 6-0sem 7-9 main-2!!! sem kob. te amo kobllux!!! artAsCii kkkkk", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
+console.log("Infodose conectado main2!! KAOS 6-0 RHEA IN 7-9 main-2!!! sem kob. te amo kobllux!!! artAsCii kkkkk", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
 import "./modules/inline-00.js";
 import "./modules/inline-1.js";
 import "./modules/inline-2.js";
 import "./modules/inline-3.js";
 import "./modules/inline-4.js";
 import "./modules/inline-5.js";
-/*
-import "./modules/inline-6-0.js";
 
+import "./modules/RHEA-bridge.js";
+import "./modules/KAOS-in6-0.js";
+import "./modules/inline-7-9.js";
+
+/*
 import "./modules/isoCos.js";
 import "./modules/KAOS-extracto.js"; 
 
-import "./modules/
+import "./modules/kob.js;
 import "./modules/inline-7-9.js";
 */
 import "./modules/inline-8.js";
