649757ea5f0090d0e4096e605c01050fcef1cca2
Kodux78k <koduxlive@gmail.com>
2026-05-20 04:20:33 -0300

    Refactor imports and add profile logging

--- M0D/KxaT/js/main.js
+++ M0D/KxaT/js/main.js
@@ -1,23 +1,26 @@
 console.log("Infodose conectado main 7-9 TE AMO DUAL! ativar Ayor ahhahah. kob depois do in6 antes do 7-9", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
-import "./modules/inline-00.js";
 
+window.KBLX_PROFILE = '7-9';
+// main-0.js
+import './modules/KBllX_ASCII_BOOT.js';
+import './modules/inline-00.js';
+import './modules/inline-1.js';
+import './modules/inline-2.js';
+import './modules/inline-3.js';
+import './modules/inline-4.js';
+import './modules/inline-5.js';
+import './modules/KOB-RHEA-KAOS-sync.js';
+import './modules/kob.js';
+import './modules/inline-7-9.js';
+import './modules/inline-8.js';
+import './modules/inline-9.js';
+import './modules/inline-10.js';
+import './modules/inline-11.js';
 
-import "./modules/inline-1.js";
-import "./modules/inline-2.js";
-import "./modules/inline-3.js";
-import "./modules/inline-4.js";
-import "./modules/inline-5.js";
-
-import "./modules/KAOS-in6-0.js";
-import "./modules/kob.js";
-
-import "./modules/inline-7-9.js";
-import "./modules/inline-8.js";
-
-
-import "./modules/inline-9.js";
-import "./modules/inline-10.js";
-
-import "./modules/inline-11.js";
-
-
+console.log(`
+╔════════════════════════════════════╗
+║ KBllX  READY                       ║
+║ PROFILE ::  7-9                    ║
+║ HASH    :: 1778•78K                ║
+╚════════════════════════════════════╝
+`);
