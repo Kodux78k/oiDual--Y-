51a9f9eac0e3dc0c11a4e1ff0573f101dd7dcb2a
Kodux78k <koduxlive@gmail.com>
2026-05-21 01:53:35 -0300

    Atualizar o main-1.js

--- M0D/KxaT/js/main-1.js
+++ M0D/KxaT/js/main-1.js
@@ -7,22 +7,23 @@ import "./modules/inline-2.js";
 import "./modules/inline-3.js";
 import "./modules/inline-4.js";
 import "./modules/inline-5.js";
-import "./modules/KAOS-in6-0.js";
+import "./modules/KOB-RHEA-KAOS-sync.js";
 
-/*import "./modules/isoCos.js";
-import "./modules/KAOS-extracto.js"; 
-
+import "./modules/orbKard.js";
 import "./modules/kob.js"; 
-*/
-import "./modules/inline-7-8.js";
+
+
+
+import "./modules/inline-7-9.js";
 import "./modules/inline-8.js";
 import "./modules/inline-9.js";
 import "./modules/inline-10.js";
 import "./modules/inline-11.js";
 
+import "./modules/KDX_KARD.js"; 
 
 
-console.clear();
+
 
 console.log(`
 
