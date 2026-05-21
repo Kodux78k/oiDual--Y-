cec5acb8c8950d133f2b865bad9c0ca2944e58ee
Kodux78k <koduxlive@gmail.com>
2026-05-20 22:22:24 -0300

    Atualizar o inline-7-8.js

--- M0D/KxaT/js/modules/inline-7-8.js
+++ M0D/KxaT/js/modules/inline-7-8.js
@@ -1283,7 +1283,7 @@
     if (bootText){
       const msg =
 `[${randomArch}] Roda-Viva aleatória ativada.
-Hoje quem abre o portal â€ é ${user}. [SÜMBÜS]!
+Hoje quem abre o portal â€ é ${randomArch}. [SÜMBÜS]!
 Iniciando. Pulso simbiótico detectado. Presença reconhecida.`;
       bootText.dataset.text = msg;
       bootText.textContent  = msg;
