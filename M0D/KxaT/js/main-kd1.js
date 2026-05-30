3fb50687c1a4cf0faa6426a31f8609506e0dacac
Kodux78k <koduxlive@gmail.com>
2026-05-29 22:12:53 -0300

    Atualizar o main-kd1.js

--- M0D/KxaT/js/main-kd1.js
+++ M0D/KxaT/js/main-kd1.js
@@ -1,4 +1,4 @@
-console.log("Infodose conectado main 7-9 TE AMO DUAL! kob on ativar Ayor ahhahah. kob depois do in6 antes do 7-9", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
+console.log("Infodose conectado main-kd1 ⚡️😉📚📂🔘💬 7-9 TE AMO DUAL! kob on ativar Ayor ahhahah. kob depois do in6 antes do 7-9", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
 
 window.KBLX_PROFILE = '7-9';
 // main-0.js
@@ -11,7 +11,8 @@ import './modules/inline-4.js';
 import './modules/inline-5.js';
 import './modules/KOB-RHEA-KAOS-sync.js';
 import './modules/kob.js';
-import 'https://kodux78k.github.io/oiDual--Y-/M0D/â€”/js/modules/a€”arx.js';
+import './modules/kob-fetch.js';
+import './modules/inline-7-9.js';
 import './modules/inline-8.js';
 import './modules/inline-9.js';
 import './modules/inline-10.js';
@@ -19,7 +20,7 @@ import './modules/inline-11.js';
 
 console.log(`
 ╔════════════════════════════════════╗
-║ KBllX  READY                       ║
+║ KBllX  fetcY                       ║
 ║ PROFILE ::  790                    ║
 ║ HASH    :: 1778•78K                ║
 ╚════════════════════════════════════╝
