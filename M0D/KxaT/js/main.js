c9a5e796c3b2f4c1046c0a899c801f9645f7d822
Kodux78k <koduxlive@gmail.com>
2026-06-01 03:00:52 -0300

    Atualizar o main.js

--- M0D/KxaT/js/main.js
+++ M0D/KxaT/js/main.js
@@ -16,7 +16,7 @@ import './modules/inline-8.js';
 import './modules/inline-9.js';
 import './modules/inline-10.js';
 import './modules/inline-11.js';
-/*import './modules/a€Arx.js';*/
+import './modules/a€Arx.js';
 console.log(`
 ╔════════════════════════════════════╗
 ║ KBllX  READY                       ║
