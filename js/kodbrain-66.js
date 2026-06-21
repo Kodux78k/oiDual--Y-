40db54d2d29c09522da4d0d4195308dab0359a9c
Kodux78k <koduxlive@gmail.com>
2026-06-20 21:28:31 -0300

    Atualizar o kodbrain-66.js

--- js/kodbrain-66.js
+++ js/kodbrain-66.js
@@ -99,7 +99,7 @@
       return `
         <svg width="${size}" height="${size}" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
           <defs>
-            <linearGradient id="${id}" x1="0%" y1="0%" x2="100%" y2="100%">
+            <linearGradient id="${gid}" x1="0%" y1="0%" x2="100%" y2="100%">
               <stop offset="0%" stop-color="hsl(${baseHue},100%,55%)"/>
               <stop offset="100%" stop-color="hsl(${accentHue},90%,45%)"/>
             </linearGradient>
